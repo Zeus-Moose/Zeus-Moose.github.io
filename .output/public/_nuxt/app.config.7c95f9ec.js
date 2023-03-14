f63c091252cefaa2f6466570fba90d68ac7af2aa
Ben <ben@zeusmoose.com>
2023-03-14 09:15:35 +0000

    UPDATES: for deploy

--- .output/public/_nuxt/app.config.7c95f9ec.js
+++
@@ -1,1 +0,0 @@
-import{x as n}from"./entry.3af56a57.js";const i={};n(i);
--- .output/server/chunks/raw/index.mjs
+++
@@ -1,5 +0,0 @@
-// ROLLUP_NO_REPLACE 
- const index = "{\"parsed\":{\"_path\":\"/\",\"_dir\":\"\",\"_draft\":false,\"_partial\":false,\"_locale\":\"\",\"_empty\":false,\"title\":\"INDEX HERE!\",\"description\":\"Hello\",\"body\":{\"type\":\"root\",\"children\":[{\"type\":\"element\",\"tag\":\"h1\",\"props\":{\"id\":\"index-here\"},\"children\":[{\"type\":\"text\",\"value\":\"INDEX HERE!\"}]},{\"type\":\"element\",\"tag\":\"p\",\"props\":{},\"children\":[{\"type\":\"text\",\"value\":\"Hello\"}]}],\"toc\":{\"title\":\"\",\"searchDepth\":2,\"depth\":2,\"links\":[]}},\"_type\":\"markdown\",\"_id\":\"content:index.md\",\"_source\":\"content\",\"_file\":\"index.md\",\"_extension\":\"md\"},\"hash\":\"QwAx3ifMmq\"}";
-
-export { index as default };
-//# sourceMappingURL=index.mjs.map
