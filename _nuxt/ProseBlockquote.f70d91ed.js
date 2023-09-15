76db0fdcac93d5caeb12fdbd511ea79404910f93
Zeus-Moose <Zeus-Moose@users.noreply.github.com>
2023-09-15 10:01:56 +0000

    deploy: dd4ac1bcb31584f82a30e7f63aa009796641e541

--- _nuxt/ProseBlockquote.f70d91ed.js
+++
@@ -1,1 +0,0 @@
-import{j as o,o as t,a as r,V as c}from"./entry.82dd30c6.js";const s={};function n(e,a){return t(),r("blockquote",null,[c(e.$slots,"default")])}const f=o(s,[["render",n]]);export{f as default};
